15717e2d1a421319300716d3c88fb2d75e10098a
Benedicte Emilie Brækken <benedicte@braekken.org>
2015-03-19 16:32:28 +0100

    Fixed listkoh to show 10 newest by ordering.

--- RTBot.py
+++ RTBot.py
@@ -165,7 +165,7 @@ class RTBot(MUCJabberBot):
 
         output = ""
         counter = 0
-        for row in c.execute('SELECT * FROM kohbesok ORDER BY date'):
+        for row in c.execute('SELECT * FROM kohbesok ORDER BY date DESC'):
             output += '%10s: %4d\n' % (row[0], int(row[1]))
             counter += 1
 
